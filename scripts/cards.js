6465480564ec8e439b70b0e15998512f2c95be1e
Jerjes Mariluz <juniormariluz1@gmail.com>
2025-09-04 10:53:07 -0500

    update

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('/data/cards.json');
+        const response = await fetch('data/cards.json');
 
         if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.statusText}`);
